d8d0726b9a097652549c75918fc4057d05bc297f
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-05 08:57:54 -0600

    asd

--- src/controllers/torrents.controller.js
+++ src/controllers/torrents.controller.js
@@ -9,7 +9,7 @@ const savetorrent=async (req,res)=>{
 try{
 
     const {test}=req.body;
-    res.json("Respondiendo desde controller Linux "+ req.body);
+    res.json("Respondiendo desde controller Linux2 "+ req.body);
     console.log(req.body);
     console.log(test);
 } catch (error) {
