8b1b971dd02f038828f6563e0830a0715b902f82
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-10 00:14:19 -0600

    tt

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -7,7 +7,7 @@ function myFunction() {
   Http.send();
   document.getElementById("botones").innerHTML='<div class="accordion" id="accordionExample">'
   //Borrar es solo para probar bootstrap
-  //document.getElementById("tests").innerHTML='<div class="accordion-item"> <h2 class="accordion-header" id="headingOne">                                                                                                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">  Primer button    </button></h2><div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample"><div class="accordion-body"><strong> Bla bla bla</strong></div></div></div>'
+  
  
   //Accordion bootstrap
 
@@ -26,16 +26,14 @@ function myFunction() {
   {
     
     
-    //document.getElementById("botones").innerHTML += '<button id="'+resp.data.movies[index].torrents[indexT].url+'" onclick="alertId(this.id)" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+index+'" aria-expanded="true" aria-controls="collapse'+index+'">' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</button></h2>';
-    document.getElementById('botones').innerHTML+='<div class="accordion-item"> <h2 class="accordion-header" id="heading'+indexT+'">      <button onclick="alertId(this.id)" id="'+resp.data.movies[index].torrents[indexT].url+'" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+indexT+'" aria-expanded="false" aria-controls="collapse'+indexT+'">  ' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'    </button></h2><div id="collapse'+indexT+'" class="accordion-collapse collapse show" aria-labelledby="heading'+indexT+'" data-bs-parent="#accordionExample"><div class="accordion-body"><strong>'+ resp.data.movies[index].torrents[indexT].summary+'</strong></div></div></div>'
+   
+    document.getElementById('botones').innerHTML+='<div class="accordion-item"> <h2 class="accordion-header" id="heading'+indexT+'">      <button onclick="alertId(this.id)" id="'+resp.data.movies[index].torrents[indexT].url+'" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+indexT+'" aria-expanded="false" aria-controls="collapse'+indexT+'">  ' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'    </button></h2><div id="collapse'+indexT+'" class="accordion-collapse collapse show" aria-labelledby="heading'+indexT+'" data-bs-parent="#accordionExample"><div class="accordion-body"><strong>'+ resp.data.movies[index].torrents[indexT].summary+'</strong></div></div></div></div>'
 
 if (indexT===0)
 console.log (document.getElementById('botones').innerHTML)   
     
     
-    //document.getElementById("botones").innerHTML += '<a href="'+resp.data.movies[index].torrents[indexT].url+'">' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</a><br>';
-    //document.getElementById("botones").innerHTML='<form>  <button type="submit" formaction="https://www.w3docs.com">ABRIR</button></form>'
-    //<button type="submit" formaction="https://www.w3docs.com">Click me</button>
+    
   
   }
   )
