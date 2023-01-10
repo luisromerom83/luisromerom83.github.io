dcd8dfd12fec9714a8990da3d544b7152b2a01d8
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-09 23:11:59 -0600

    accordion1

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -5,7 +5,15 @@ function myFunction() {
   const url='https://yts.mx/api/v2/list_movies.jsonp?quality=1080&query_term='+document.getElementById("movie").value;
   Http.open("GET", url);
   Http.send();
-  document.getElementById("botones").innerHTML=""
+  document.getElementById("botones").innerHTML='<div class="accordion" id="accordionExample">'
+  
+//Accordion bootstrap
+
+
+  
+//Fin Accordion bootrstrap
+
+  
   Http.onreadystatechange = (e) => {
     
     //document.getElementById("respuesta").innerHTML = Http.responseText;
@@ -14,9 +22,11 @@ function myFunction() {
   
   resp.data.movies[index].torrents.forEach((element,indexT)=>
   {
-    
+    document.getElementById("botones").innerHTML+='<div class="accordion-item"> <h2 class="accordion-header" id="heading"'+index+'">'
     //document.getElementById("botones").innerHTML += '<button type="submit" formaction="'+resp.data.movies[index].torrents[indexT].url+'">' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</button><br>';
-    document.getElementById("botones").innerHTML += '<button id="'+resp.data.movies[index].torrents[indexT].url+'" onclick="alertId(this.id)" >' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</button><br>';
+    document.getElementById("botones").innerHTML += '<button id="'+resp.data.movies[index].torrents[indexT].url+'" onclick="alertId(this.id)" >' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</button></h2>';
+    document.getElementById("botones").innerHTML+=' <div id="collapse'+index+'" class="accordion-collapse collapse show" aria-labelledby="heading'+index+'" data-bs-parent="#accordionExample"><div class="accordion-body">'+resp.data.movies[index].summary+'</div></div></div>'
+    
     //document.getElementById("botones").innerHTML += '<a href="'+resp.data.movies[index].torrents[indexT].url+'">' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'</a><br>';
     //document.getElementById("botones").innerHTML='<form>  <button type="submit" formaction="https://www.w3docs.com">ABRIR</button></form>'
     //<button type="submit" formaction="https://www.w3docs.com">Click me</button>
@@ -30,7 +40,7 @@ function myFunction() {
     
   
   }
-  //document.getElementById("botones").innerHTML +="</form>"
+  document.getElementById("botones").innerHTML +="</div>"
   }
   catch (error) {
       console.log(error.message);
