07b75e875faf525b5fded4e175070e33d4aac3f3
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-10 11:58:36 -0600

    as

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -35,7 +35,7 @@ function myFunction() {
       //Get genres repeating subarray
       let genres="";
       resp.data.movies[index].genres.forEach((element,indexG)=>{
-        genres+='<span class="badge text-bg-primary">Género: '+resp.data.movies[index].genres[indexG].Value+'</span>'
+        genres+='<span class="badge text-bg-primary">Género: '+resp.data.movies[index].genres[indexG].'Value'+'</span>'
       
       });
       
