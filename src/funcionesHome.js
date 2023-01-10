ef4b93cd99cedb03d0fd1637e20d579dc85306dd
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-10 00:32:39 -0600

    as

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -5,7 +5,7 @@ function myFunction() {
   const url='https://yts.mx/api/v2/list_movies.jsonp?quality=1080&query_term='+document.getElementById("movie").value;
   Http.open("GET", url);
   Http.send();
-  document.getElementById("botones").innerHTML='<div class="accordion" id="accordionExample">'
+  //document.getElementById("botones").innerHTML='<div class="accordion" id="accordionExample">'
   //Borrar es solo para probar bootstrap
   
  
@@ -27,10 +27,10 @@ function myFunction() {
     
     
    
-    document.getElementById('botones').innerHTML+='<div class="accordion-item"> <h2 class="accordion-header" id="heading'+indexT+'">      <button onclick="alertId(this.id)" id="'+resp.data.movies[index].torrents[indexT].url+'" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+indexT+'" aria-expanded="false" aria-controls="collapse'+indexT+'">  ' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'    </button></h2><div id="collapse'+indexT+'" class="accordion-collapse collapse show" aria-labelledby="heading'+indexT+'" data-bs-parent="#accordionExample"><div class="accordion-body"><strong>'+ resp.data.movies[index].torrents[indexT].summary+'XXX</strong></div></div></div>'
+    document.getElementById('botones').innerHTML+='<div class="accordion" id="accordionExample"><div class="accordion-item"> <h2 class="accordion-header" id="heading'+indexT+'">      <button onclick="alertId(this.id)" id="'+resp.data.movies[index].torrents[indexT].url+'" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+indexT+'" aria-expanded="false" aria-controls="collapse'+indexT+'">  ' + resp.data.movies[index].title + ' '+resp.data.movies[index].torrents[indexT].quality+'    </button></h2><div id="collapse'+indexT+'" class="accordion-collapse collapse show" aria-labelledby="heading'+indexT+'" data-bs-parent="#accordionExample"><div class="accordion-body"><strong>'+ resp.data.movies[index].torrents[indexT].summary+'XXX</strong></div></div></div></div>'
 
- if ((index+1)==resp.data.movies.length && (indexT+1)==resp.data.movies[index].torrents.length)
- document.getElementById('botones').innerHTML+='</div><h2>Hola Div</h2>';
+ //if ((index+1)==resp.data.movies.length && (indexT+1)==resp.data.movies[index].torrents.length)
+ //document.getElementById('botones').innerHTML+='</div><h2>Hola Div</h2>';
     
     
   
