a504a1d29e6114970e69923bfc25e103ec972d40
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-10 11:54:48 -0600

    asd

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -46,7 +46,7 @@ function myFunction() {
     //Botón header
     botoneshtml+='<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+movieid+'" aria-expanded="false" aria-controls="collapse'+movieid+'">  ';
     //Texto botón header
-    botoneshtml+= resp.data.movies[index].title +' '+genres+' </button></h2>';
+    botoneshtml+= resp.data.movies[index].title +'         '+genres+' </button></h2>';
     //Inicio body
     botoneshtml+='<div id="collapse'+movieid+'" class="accordion-collapse collapse show" aria-labelledby="heading'+movieid+'" data-bs-parent="#accordionExample">';
     //Contenido body
