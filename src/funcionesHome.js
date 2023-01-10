fca009e78c15a7ac63a71768a42172f60f01300a
luisromerom83 <121853580+luisromerom83@users.noreply.github.com>
2023-01-10 00:38:46 -0600

    asd

--- src/funcionesHome.js
+++ src/funcionesHome.js
@@ -18,10 +18,10 @@ function myFunction() {
   
   Http.onreadystatechange = (e) => {
     
-    //document.getElementById("respuesta").innerHTML = Http.responseText;
+    document.getElementById('botones').innerHTML='';
     const resp=JSON.parse(Http.responseText)
     resp.data.movies.forEach((element,index) => {
-      document.getElementById('botones').innerHTML='';
+      
   resp.data.movies[index].torrents.forEach((element,indexT)=>
   {
     
